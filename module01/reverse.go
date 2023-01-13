b27ff7a7ad26c91b7a591e9c06e5da54f6e83f02
James Olds <james@oldsmail.com>
2023-01-12 21:10:43 -0500

    utf8 solution

--- module01/reverse.go
+++ module01/reverse.go
@@ -1,7 +1,5 @@
 package module01
 
-import "fmt"
-
 // Reverse will return the provided word in reverse
 // order. Eg:
 //
@@ -10,8 +8,8 @@ import "fmt"
 //
 func Reverse(word string) string {
 	var reverse string
-	for i := range word {
-		reverse += string(word[len(word)-i-1])
+	for _, r := range word {
+		reverse = string(r) + reverse
 	}
 	return reverse
 }
